a5addadffd0de0d90392a4bdd76bb6d901922299
federicoburman <fburman@fi.uba.ar>
2020-11-10 21:09:30 +0000

    commit final

--- main.c
+++ main.c
@@ -13,7 +13,6 @@
 
 #define STD_FILE "-"
 
-#define DELIMITER " "
 #define ENDLINE '\n'
 #define NULLTER '\0'
 
@@ -25,9 +24,8 @@
 #define MODO_INPUT_OUTPUT 5
 #define MODO_OUTPUT_INPUT 6
 #define MODO_STD 7
-/*-------------------------------------------------------*/
 
-/*----------------------get-flags------------------------*/
+//COMPROBAR FLAGS
 bool is_help_flag(const char* flag) {
     return (strcmp(flag, "-h") == 0 || strcmp(flag,"--help") == 0);
 }
@@ -36,17 +34,16 @@ bool is_version_flag(const char* flag) {
     return (strcmp(flag, "-V") == 0 || strcmp(flag, "--version") == 0);
 }
 
-bool is_input_flag(const char* flag) {
+bool es_input(const char* flag) {
     return (strcmp(flag, "-i") == 0 || strcmp(flag, "--input") == 0);
 }
 
-bool is_output_flag(const char* flag) {
+bool es_output(const char* flag) {
     return (strcmp(flag, "-o") == 0 || strcmp(flag, "--output") == 0);
 }
-/*-------------------------------------------------------*/
 
 
-/*-----------------get-exectuion-modes-------------------*/
+//Obtener modo de ejecicion
 int modo_2_argumentos(char* const argv[]) {
     const char* flag = argv[1];
 
@@ -62,10 +59,10 @@ int modo_2_argumentos(char* const argv[]) {
 int modo_3_argumentos(char* const argv[]) {
     const char* flag = argv[1];
 
-    if (is_input_flag(flag))
+    if (es_input(flag))
         return MODO_INPUT;
 
-    if (is_output_flag(flag))
+    if (es_output(flag))
         return O_MODE;
 
     return ERROR;
@@ -75,18 +72,19 @@ int modo_5_argumentos(char* const argv[]) {
     const char* flag_1 = argv[1];
     const char* flag_2 = argv[3];
 
-    bool input_flag_frt = is_input_flag(flag_1);
-    bool input_flag_scd = is_input_flag(flag_2);
+    bool flag_1_input = es_input(flag_1);
+    bool flag_2_input = es_input(flag_2);
 
-    bool output_flag_frt = is_output_flag(flag_1);
-    bool output_flag_scd = is_output_flag(flag_2);
+    bool flag_1_output = es_output(flag_1);
+    bool flag_2_output = es_output(flag_2);
 
-    if (input_flag_frt && output_flag_scd)
+    if (flag_1_input && flag_2_output)
         return MODO_INPUT_OUTPUT;
 
-    if (output_flag_frt && input_flag_scd)
+    if (flag_1_output && flag_2_input)
         return MODO_OUTPUT_INPUT;
 
+    
     return ERROR;
 }
 
@@ -105,9 +103,8 @@ int modo_ejecucion(int argc, char* const argv[]) {
     }
 }
 
+//MODOS DE EJECUCION
 
-
-/*--------------------exectuion-modes--------------------*/
 int help() {
     printf("Usage:\n");
     printf("\ttp1 -h\n");
@@ -130,11 +127,11 @@ int version() {
 }
 
 
-
+//MAIN
 
 int main(int argc, char* const argv[]) {
-    const char* i_filename = STD_FILE; 
-    const char* o_filename = "salida_default.txt"; 
+    const char* nombre_archivo_input = STD_FILE; 
+    const char* nombre_archivo_output = "salida_default.txt"; 
 
     int mode = modo_ejecucion(argc, argv);
     switch (mode) {
@@ -145,21 +142,21 @@ int main(int argc, char* const argv[]) {
         case MODO_STD:
             break;
         case MODO_INPUT: {
-            i_filename = argv[2];
+            nombre_archivo_input = argv[2];
             break;
         }
         case O_MODE: {
-            o_filename = argv[2];
+            nombre_archivo_output = argv[2];
             break;
         }
         case MODO_INPUT_OUTPUT: {
-            i_filename = argv[2];
-            o_filename = argv[4];
+            nombre_archivo_input = argv[2];
+            nombre_archivo_output = argv[4];
             break;
         }
         case MODO_OUTPUT_INPUT: {
-            i_filename = argv[4];
-            o_filename = argv[2];
+            nombre_archivo_input = argv[4];
+            nombre_archivo_output = argv[2];
             break;
         }
         default: {
@@ -169,11 +166,11 @@ int main(int argc, char* const argv[]) {
     }
     
 
-    bool input_is_std = (strcmp(i_filename, STD_FILE) == 0);
-    bool output_is_std = (strcmp(o_filename, STD_FILE) == 0);
+    bool input_is_std = (strcmp(nombre_archivo_input, STD_FILE) == 0);
+    bool output_is_std = (strcmp(nombre_archivo_output, STD_FILE) == 0);
    
     if (input_is_std) {
-       	   if(!output_is_std) fopen(o_filename, "w+"); 
+       	   if(!output_is_std) fopen(nombre_archivo_output, "w+"); 
            printf( "No se ingreso un archivo de entrada, para mas ayuda, usar comando -h\n");
            return ERROR;
         
@@ -182,8 +179,8 @@ int main(int argc, char* const argv[]) {
    
 
     file_reader_t file_reader;
-    file_init(&file_reader,i_filename);
-    read_hash_file(&file_reader, o_filename);
+    file_init(&file_reader,nombre_archivo_input);
+    read_hash_file(&file_reader, nombre_archivo_output);
     file_reader_uninit(&file_reader);
 
     return SUCCESS;
